baa21ced20e7ee186d5aba1e62b3391468052a8b
Nicholas Mattei <nsmattei@gmail.com>
2020-09-08 17:26:30 -0500

    fixed new path

--- run_script_server.py
+++ run_script_server.py
@@ -7,19 +7,19 @@ import glob
 
 if __name__ == "__main__":
 	#path = '''./json/for\\ AAAI21\\ submission/AAAI17_sim/'''
-	files = [ 	"./json/for\\ AAAI21\\ submission/AAAI17_sim/AAAI17_1200X300_orig.json",
-			  	"./json/for\\ AAAI21\\ submission/AAAI17_sim/AAAI17_2000X200_orig.json",
-				"./json/for\\ AAAI21\\ submission/AAAI17_sim/AAAI17_600X400_orig.json",
-				"./json/for\\ AAAI21\\ submission/ICLR_sim/ICLR_150X100_greedy.json",
-				"./json/for\\ AAAI21\\ submission/ICLR_sim/ICLR_150X100_uniform.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_176X146_orig.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_176X146_uniform.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_442X161_greedy.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_442X161_orig.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_442X161_uniform.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_613X201_greedy.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_613X201_orig.json",
-				"./json/for\\ AAAI21\\ submission/PrefLib_sim/PrefLib_613X201_uniform.json"
+	files = [ 	"./json/for_AAAI21_submission/AAAI17_sim/AAAI17_1200X300_orig.json",
+			  	"./json/for_AAAI21_submission/AAAI17_sim/AAAI17_2000X200_orig.json",
+				"./json/for_AAAI21_submission/AAAI17_sim/AAAI17_600X400_orig.json",
+				"./json/for_AAAI21_submission/ICLR_sim/ICLR_150X100_greedy.json",
+				"./json/for_AAAI21_submission/ICLR_sim/ICLR_150X100_uniform.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_176X146_orig.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_176X146_uniform.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_442X161_greedy.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_442X161_orig.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_442X161_uniform.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_613X201_greedy.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_613X201_orig.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_613X201_uniform.json"
 	         ]
 	run_cmd = "python3 ./simulation.py"
 	for file in files:
