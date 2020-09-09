419f8d86f863a846d865f517b4ba2ef3db0a89fc
Nicholas Mattei <nsmattei@gmail.com>
2020-09-09 10:55:09 -0500

    next round

--- run_script_server.py
+++ run_script_server.py
@@ -7,10 +7,12 @@ import glob
 
 if __name__ == "__main__":
 	#path = '''./json/for\\ AAAI21\\ submission/AAAI17_sim/'''
-	files = [ 	"./json/for_AAAI21_submission/AAAI17_sim/AAAI17_600X400_orig_U.json",
-				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_613X201_orig_U.json",
-				"./json/for_AAAI21_submission/ICLR_sim/ICLR_600X400_uniform_U.json",
-				"./json/for_AAAI21_submission/ICLR_sim/ICLR_900X300_uniform_U.json",
+	files = [   "./json/for_AAAI21_submission/PrefLib_sim/PrefLib_52X24_greedy.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_52X24_orig.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_52X24_uniform.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_54X31_greedy.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_54X31_orig.json",
+				"./json/for_AAAI21_submission/PrefLib_sim/PrefLib_54X31_uniform.json",
 				
 	         ]
 	run_cmd = "python3 ./simulation.py"
